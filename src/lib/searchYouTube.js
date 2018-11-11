d30f67c585b8752780b1226835888a6ff6e8fb28
Clifton Ho <cliftonho311@gmail.com>
2018-11-11 15:03:26 -0800

    Complete creating reusable API helper

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,27 @@
-var searchYouTube = (options, callback) => {
+var searchYouTube = (options, successCB) => {
   // TODO
+  $.ajax({
+    type: 'GET',
+    url: "https://www.googleapis.com/youtube/v3/search",
+    data: { 
+      "q": options.query, 
+      "maxResults": options.max || '5', 
+      "videoEmbeddable": 'true', 
+      "type": 'video',
+      "part": 'snippet',
+      "key": options.key
+    },
+    success: function(data) {
+      successCB(data.items);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
+
+// data: { 
+//   "q": options.query, 
+//   "maxResults": options.max, 
+//   "type": 'video', 
+//   "key": options.key
+// },
